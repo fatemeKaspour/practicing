7ad876f04d180abc010bf0510534d296a9fd72cc
unknown <you@gmail.com>
2021-12-14 12:28:31 +0330

    fix some html codes

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
-const addition = (number1 , number2) => number1 + number2;
-const result = addition(2,5);
-console.log(result);
+//afunction to show firstName with alert
+function UserName (firstName , func){
+    func(firstName);
+}
+UserName('fateme' , alert);
+//an arrow function
+let SumNumbers =(num1 , num2) => num1 + num2;
+console.log(SumNumbers(2,6));

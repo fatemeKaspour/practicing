4aa97746c299c7db50cd948cbb25d875203e8903
unknown <you@gmail.com>
2021-12-16 11:23:21 +0330

    convert minutes to seconds

--- main.js
+++ main.js
@@ -7,3 +7,6 @@ function userName (firstName , func){
 //an arrow function for sum two numbers
 let sumNumbers =(num1 , num2) => num1 + num2;
 // console.log(sumNumbers(2,6));
+//convert minutes to seconds
+let convertToMinutes = (minutes)=> minutes*60;
+// console.log(convertToMinutes(1));

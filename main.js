4b575cea03d177c12bdaf63091afc6dbab5b9754
unknown <you@gmail.com>
2021-12-16 11:22:27 +0330

    an arrow function for sum two numbers

--- main.js
+++ main.js
@@ -4,3 +4,6 @@ function userName (firstName , func){
     func(firstName);
 }
 // userName('fateme' , alert);
+//an arrow function for sum two numbers
+let sumNumbers =(num1 , num2) => num1 + num2;
+// console.log(sumNumbers(2,6));

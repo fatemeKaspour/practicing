aa1b1810db279914f66b6b15199ab6c1f237df46
unknown <you@gmail.com>
2021-12-16 11:28:58 +0330

    make befor conditional function with ? and : and arrow function

--- main.js
+++ main.js
@@ -14,13 +14,17 @@ let convertToMinutes = (minutes)=> minutes*60;
 let areaTriangle = (height , weight) => (height*weight)/2
 // console.log(areaTriangle(50,20));
 //a function to show sum of two numbers is less than 100 or not
-function isLessThan100(a , b){
-    let sum = a + b ;
-    if(sum <100){
-        return true;
-    }else{
-        return false;
-    }
-}
+// function isLessThan100(a , b){
+//     let sum = a + b ;
+//     if(sum <100){
+//         return true;
+//     }else{
+//         return false;
+//     }
+// }
+// const result = isLessThan100(60,70);
+// console.log(result);
+//make befor conditional function with ? and : and arrow function
+let isLessThan100 = (a , b) => (a + b >100) ? false : true;
 const result = isLessThan100(60,70);
 // console.log(result);

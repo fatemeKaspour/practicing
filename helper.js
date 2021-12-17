1fe3240c8f3bd7b13d6df96b189d5de597dea703
unknown <you@gmail.com>
2021-12-17 13:48:54 +0330

    function by name creatElement created

--- helper.js
+++ helper.js
@@ -9,5 +9,22 @@
     * @param {Object} attribute
     * @param {HTMLElement | String | Array} content
     */
+    // ******************function creatElement
+
+   function creatElement (tagName , attribute , content){
+       var elem = document.createElement(tagName);
+       for(var key in attribute){
+           elem.setAttribute(key , attribute[key]);
+       }
+       if (typeof content !== "undefined"){
+            if (content instanceof HTMLElement){
+                elem.appendChild(content);
+            }else{
+                elem.innerText = content;
+            };
+       }
+       return elem;
+       
+   }
 
 })();
